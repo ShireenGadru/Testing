e598dfe67735e87d5401b0fcee777c5d390bcc03
Shireen Gadru <shiru411@gmail.com>
2022-07-14 19:09:36 +0530

    test and code for scoops subtotal

--- sundaes-on-demand/src/App.js
+++ sundaes-on-demand/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
-import Options from "./pages/entry/Options";
-import SummaryForm from "./pages/summary/SummaryForm";
+import Container from "react-bootstrap/Container";
+import OrderEntry from "./pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 function App() {
   return (
-    <div className="App">
-      <SummaryForm />
-      <Options />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider  */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider  */}
+    </Container>
   );
 }
 
--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -4,16 +4,22 @@ import ScoopOption from "./ScoopOption";
 import Row from "react-bootstrap/Row";
 import ToppingOption from "./ToppingOption";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
-      .then((res) => setItems(res.data))
+      .then((res) => {
+        console.log(res.data);
+        setItems(res.data);
+      })
       .catch((error) => {
         setError(true);
       });
@@ -25,16 +31,29 @@ const Options = ({ optionType }) => {
   }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) => {
+        updateItemCount(itemName, newItemCount, optionType);
+      }}
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]}</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
